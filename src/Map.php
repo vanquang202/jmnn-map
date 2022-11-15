5a4ebdbe696055abb1273a5889c75ed8635f6067
vanquang202 <kopopi593@gmail.com>
2022-11-15 11:24:53 +0700

    Update

--- src/Map.php
+++ src/Map.php
@@ -8,27 +8,77 @@ use Str;
 class Map implements MapInterface
 {
 
-    public function build($data = [] ,$search = '', $key = 'name')
+    public function build($data = [] ,$search = '', $key = 'name' , $arrayKeyTree = ['district','town'])
     {
-        return array_filter($data,function ($data) use ($search,$key){
-            $name = $this->__convertString($data[$key]);
-            return (bool) $this->__macth($search,$name);
+
+        return array_filter($data,function ($data) use ($search , $key, $arrayKeyTree) {
+            return $this->__callback($data, $search, $key, $arrayKeyTree);
         });
 
     }
 
-    private function __convertString($data)
-    {
-        return Str::squish(Str::slug($data,' '));
+    private function __callback($data , $search , $key , $arrayKeyTree){
+        $name = $data[$key];
+        $flag = false;
+        if($keyData = $this->__checkKeyArray($data, $arrayKeyTree)) $flag = true ;
+        return (bool) $this->__match($search,$name) ?: ($flag ? $this->__callback($data[$keyData], $search, $key, $arrayKeyTree) : false);
     }
 
-    private function __macth($search , $str)
+    private function __checkKeyArray ($data ,$arrayKeyTree )
     {
-        $patternString = implode(")|(",array_map(function ($data) {
-            return $this->__convertString($data);
-        },explode(",",$search)));
+        foreach ($arrayKeyTree as $key)
+        {
+            if(isset($data[$key])) return $key;
+        }
+        return false;
+    }
+
+    private function __convertString($data)
+    {
+        return Str::squish($data);
+    }
+
+    private function __match($search , $str)
+    {
+
+        $ex = explode(",",$search);
+        $flag = false;
+        $patternString = $this->__cvPatternString($ex);
 
         $pattern = '/('.$patternString.')/i';
-        return (bool) preg_match($pattern,$str);
+
+        $searchNew = $this->__cvPatternNewString($ex);
+
+        if(trim($searchNew) == "") $flag = true;
+        return (bool) preg_match($pattern,$str) ?: ($flag ? false :  $this->__match($searchNew , $str));
+
+    }
+
+    private function __cvPatternString($ex)
+    {
+
+        return  implode(")|(",array_map(function ($data) {
+
+            $exT = preg_split("/\s+/",$this->__convertString($data));
+            $array = array_chunk($exT,2);
+            $result = implode(")|(",array_map(function ($data) {
+                if(count($data) > 1) return implode(" ",$data);
+                return "Default";
+            },$array));
+            return $this->__convertString("$result");
+
+        },$ex));
+
+    }
+
+    private function __cvPatternNewString($ex)
+    {
+        return trim(implode(",",array_map(function ($data)   {
+            $exT = preg_split("/\s+/",$this->__convertString($data));
+            unset($exT[0]);
+            if(count($exT) < 2) return "";
+            return implode(" ",$exT);
+        },$ex)),",");
+
     }
 }
